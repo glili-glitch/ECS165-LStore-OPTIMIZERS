31e3daa22867391c6e356dc424843d484fa5b1e8
Lillian Li <glili@ucdavis.edu>
2026-02-24 15:08:03 -0800

    fixing no change

--- lstore/bufferpool.py
+++ lstore/bufferpool.py
@@ -73,16 +73,12 @@ class BufferPool:
 
     def _ensure_capacity(self):
         """Evict LRU unpinned pages until pool is under capacity."""
-        while len(self.pool) > self.pool_size:
+        while len(self.pool) >= self.pool_size:
             # without creating a full list and get the oldest page(LRU)
-            it = iter(self.pool) 
             evicted = False
 
             # we can check oldest are pinned
-            for _ in range(len(self.pool)):
-                pid = next(it)
-
-            # for pid in list(self.pool.keys()):       # Iterate LRU → MRU
+            for pid in list(self.pool.keys()):       # Iterate LRU → MRU
                 if not self.is_pinned(pid):
                     self._evict(pid)
                     evicted = True
@@ -123,9 +119,8 @@ class BufferPool:
         if page_path:
             with open(page_path, 'wb') as f:
                 # optimization write the byt directly
-                f.write(page.data)
-        if self.data_file:
-            pass
+                f.write(bytes(page.data))
+        if self.data_file
 
     def _get_page_path(self, page_id):
         if self.disk_path is None:
