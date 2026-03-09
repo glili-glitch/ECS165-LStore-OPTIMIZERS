9672bb705b2727972beed32de5f349fa3a484fdc
melissa benbala <mbenbala@ucdavis.edu>
2026-03-09 01:04:35 -0700

    small change in transaction worker

--- lstore/transaction_worker.py
+++ lstore/transaction_worker.py
@@ -1,37 +1,29 @@
 import threading
-from lstore.table import Table, Record
-from lstore.index import Index
+
 
 class TransactionWorker:
+    """
+    Creates a transaction worker object.
+    """
 
-    """
-    # Creates a transaction worker object.
-    """
-    def __init__(self, transactions = None):
+    def __init__(self, transactions=None):
         self.stats = []
         if transactions is None:
             self.transactions = []
-            self.transactions = transactions
         else:
             self.transactions = transactions
         self.result = 0
         self.thread = None
-        pass
 
-    
-    """
-    """
     def add_transaction(self, t):
         self.transactions.append(t)
 
-        
     """
-    Runs all transaction as a thread
+    Runs all transactions as a thread
     """
     def run(self):
         self.thread = threading.Thread(target=self.__run)
         self.thread.start()
-    
 
     """
     Waits for the worker to finish
@@ -40,15 +32,10 @@ class TransactionWorker:
         if self.thread:
             self.thread.join()
 
-
     def __run(self):
-        for i, transaction in enumerate(self.transactions):
-            while True:
-                success = transaction.run()
-                if success:
-                    self.stats.append(True)
-                    break
-            
-        # stores the number of transactions that committed
-        self.result = len(list(filter(lambda x: x, self.stats)))
+        for transaction in self.transactions:
+            success = transaction.run()
+            self.stats.append(success)
 
+        # stores the number of transactions that committed
+        self.result = sum(1 for x in self.stats if x)
